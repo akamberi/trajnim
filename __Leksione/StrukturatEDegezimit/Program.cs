4f471d31e89fff89d9d12a651cc0a3eeee1bf66e
Afrim Kamberi <afrimkamberi2@gmail.com>
2025-10-06 19:34:22 +0200

    Leksion 2

---
+++ __Leksione/StrukturatEDegezimit/Program.cs
@@ -0,0 +1,11 @@
+﻿var nr1 = 5;
+var nr2 = 5;
+var nr3 = 5;
+if (nr1 > nr2 && nr1 > nr3)
+{
+    Console.WriteLine("Nr1 eshte me i madhi");
+}
+else
+{
+    Console.WriteLine("Nr1 nuk eshte me i madhi");
+}
