c3d7a4bb93f17e8501d0dbd7d70e1edc84207890
Afrim Kamberi <afrimkamberi2@gmail.com>
2025-10-06 19:39:13 +0200

    Update Program.cs

--- __Leksione/StrukturatEDegezimit/Program.cs
+++ __Leksione/StrukturatEDegezimit/Program.cs
@@ -6,7 +6,11 @@ if (nr1 > nr2 && nr1 > nr3)
 {
     Console.WriteLine("Nr1 eshte me i madhi");
 }
+else if(nr2 > nr3)
+{
+    Console.WriteLine("Nr2 eshte me i madhi");
+}
 else
 {
-    Console.WriteLine("Nr1 nuk eshte me i madhi");
+    Console.WriteLine("Nr3 eshte me i madhi");
 }
