70761e897791b40d5233cf985b8785710f177b20
marinelbardhaj08-dev <marinelbardhaj08@gmail.com>
2025-10-09 17:53:20 +0200

    Create Program.cs

---
+++ ushtrime Marinel/leksioni2ushtrimi3/leksioni2ushtrimi3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("jep shumefishin : ");
+int shumefish = int.Parse(Console.ReadLine());
+Console.Write("jep nje numrin : ");
+int.TryParse(Console.ReadLine(), out int numri);
+if (shumefish % numri == 0)
+{
+    Console.WriteLine("eshte shumefish");
+}
+else
+{
+    Console.WriteLine("nuk eshte shumefish");
+}
