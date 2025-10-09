014ac619c3bf51f0efc25df0d45727fbe3a0f5d1
marinelbardhaj08-dev <marinelbardhaj08@gmail.com>
2025-10-09 10:22:55 +0200

    Create Program.cs

---
+++ ushtrime Marinel/leksioni2ushtrimi4/leksioni2ushtrimi4/Program.cs
@@ -0,0 +1,17 @@
+﻿
+Console.Write("jep numrin a : ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("jep mumrin b : ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("jep numrin c : ");
+int c = int.Parse(Console.ReadLine());
+int mesatarja = (a + b + c) / 3;
+Console.WriteLine("mesatarja eshte : " + mesatarja);
+if (mesatarja > 10)
+{
+    Console.WriteLine("mesatarja eshte me madhe se 10 ");
+}
+else
+{
+    Console.WriteLine("mesatarja eshte me e vogle se 10");
+}
