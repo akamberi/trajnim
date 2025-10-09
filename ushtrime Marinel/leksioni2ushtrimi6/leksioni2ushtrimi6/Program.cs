51523cbc7cf071319e09c394ce29cd3877bbba23
marinelbardhaj08-dev <marinelbardhaj08@gmail.com>
2025-10-09 11:41:25 +0200

    Create Program.cs

---
+++ ushtrime Marinel/leksioni2ushtrimi6/leksioni2ushtrimi6/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("jep emrin figures gjeometrike: ");
+string fig = Console.ReadLine();
+switch (fig)
+{
+    case "katror":
+        Console.WriteLine("siperfaqa = a * a");
+        Console.WriteLine("perimetri = 4 * a");
+        break;
+    case "rreth":
+        Console.WriteLine("siperfaqa = pi * (r * r) ");
+        Console.WriteLine("perimetri = 2 * pi * r");
+        break;
+    case "drejtkendesh":
+        Console.WriteLine("perimetri = 2 * (gjatesi + gjeresi) ");
+        Console.WriteLine("siperfaqa = gjatesi * gjeresi ");
+        break;
+    default:
+        Console.WriteLine("e panjohur");
+        break;
+}
