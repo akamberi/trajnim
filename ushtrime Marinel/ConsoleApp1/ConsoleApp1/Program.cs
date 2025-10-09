64870e5afd8b6ae55ac62b0bbbd109f5b095f7b8
marinelbardhaj08-dev <marinelbardhaj08@gmail.com>
2025-10-09 10:13:59 +0200

    Create Program.cs

---
+++ ushtrime Marinel/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Shkruani moshën tuaj: ");
+int mosha = int.Parse(Console.ReadLine());
+
+if (mosha >= 0 && mosha <= 12)
+{
+    Console.WriteLine("Jeni Fëmijë.");
+}
+else if (mosha >= 13 && mosha <= 17)
+{
+    Console.WriteLine("Jeni Adoleshent.");
+}
+else if (mosha >= 18 && mosha <= 64)
+{
+    Console.WriteLine("Jeni i Rritur.");
+}
+else if (mosha >= 65)
+{
+    Console.WriteLine("Jeni i Moshuar.");
+}
+else
+{
+    Console.WriteLine("Moshe e pavlefshme!");
+}
