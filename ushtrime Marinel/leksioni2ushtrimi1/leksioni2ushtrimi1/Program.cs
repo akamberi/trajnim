bf35692f513694ba4e13a7af48cfcd086cabaf61
marinelbardhaj08-dev <marinelbardhaj08@gmail.com>
2025-10-09 10:17:35 +0200

    Create Program.cs

---
+++ ushtrime Marinel/leksioni2ushtrimi1/leksioni2ushtrimi1/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("jep nje numrin : ");
+int.TryParse(Console.ReadLine(), out int numri);
+if (numri / 5 == 1)
+{
+    Console.WriteLine("plotepjestohet me 5");
+}
+else
+{
+    Console.WriteLine(" nuk plotepjestohet me 5");
+}
