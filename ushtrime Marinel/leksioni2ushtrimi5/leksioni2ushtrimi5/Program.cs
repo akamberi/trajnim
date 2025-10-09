8bfb7712336752d4d7e7899db98c6f17226f0a46
marinelbardhaj08-dev <marinelbardhaj08@gmail.com>
2025-10-09 10:25:50 +0200

    Create Program.cs

---
+++ ushtrime Marinel/leksioni2ushtrimi5/leksioni2ushtrimi5/Program.cs
@@ -0,0 +1,30 @@
+﻿
+Console.Write("jep numrin: ");
+int.TryParse(Console.ReadLine(), out int a);
+switch (a)
+{
+    case 1:
+        Console.WriteLine("pune");
+        break;
+    case 2:
+        Console.WriteLine("pune");
+        break;
+    case 3:
+        Console.WriteLine("pune");
+        break;
+    case 4:
+        Console.WriteLine("pune");
+        break;
+    case 5:
+        Console.WriteLine("pune");
+        break;
+    case 6:
+        Console.WriteLine("fundjave");
+        break;
+    case 7:
+        Console.WriteLine("fundjave");
+        break;
+    default:
+        Console.WriteLine("e panjohur");
+        break;
+}
