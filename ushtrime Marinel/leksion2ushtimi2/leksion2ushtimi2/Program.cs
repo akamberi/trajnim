5456fc7c06a185abf258140adb2f9682c266dcde
marinelbardhaj08-dev <marinelbardhaj08@gmail.com>
2025-10-09 10:20:11 +0200

    Create Program.cs

---
+++ ushtrime Marinel/leksion2ushtimi2/leksion2ushtimi2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("vendos moshen : ");
+int.TryParse(Console.ReadLine(), out int mosha);
+if (mosha >= 18)
+{
+    Console.WriteLine("mund te votoj");
+}
+else
+{
+    Console.WriteLine("nuk mund te votoj");
+}
