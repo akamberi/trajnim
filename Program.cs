7c5f7a69f3dc7f0845b6259d2661779cf802ff06
uruci877 <augerturuci6@gmail.com>
2025-10-10 21:20:02 +0200

    ushtrime Augert

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Ky program kontrollon nese nje numer eshte shumefish i 3 dhe 5
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Jep nje numer: ");
+        int numri = int.Parse(Console.ReadLine());
+
+        if (numri % 3 == 0 && numri % 5 == 0)
+        {
+            Console.WriteLine("Numri eshte shumefish i 3 dhe 5.");
+        }
+        else
+        {
+            Console.WriteLine("Numri nuk eshte shumefish i 3 dhe 5.");
+        }
+    }
+}
