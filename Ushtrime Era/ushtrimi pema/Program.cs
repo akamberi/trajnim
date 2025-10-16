6b2800c926b03b9c229497360ce8dd26cecdf21c
Ermen2005 <ermena.qorraj15@gmail.com>
2025-10-16 16:21:53 +0200

    Ushtrimi -Pema e krishtlindjeve

---
+++ Ushtrime Era/ushtrimi pema/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("jeo numrin e rreshtave:"); 
+int.TryParse(Console.ReadLine(), out int n);
+
+for (int i = 1; i <= n; i++)
+{
+    for (int j = 1; j <= n - i; j++)
+    {
+        Console.Write(" ");
+    }
+    for (int k = 1; k <= (2 * i - 1); k++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
