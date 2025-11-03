49c02a1e395bcf31b79819b8486e91c31ab1e1b1
kristjana2000 <kristjana.qorraj@fshnstudent.info>
2025-11-03 20:05:54 +0100

    Listat_Perseritje

---
+++ __Leksione/listat/listat/Libri.cs
@@ -0,0 +1,16 @@
+﻿public class Libri
+{
+    public string Titulli { get; set; }
+    public string Autori { get; set; }
+    public int Viti { get; set; }
+    public static void Afisho(List<Libri> librat)
+    {
+        foreach (Libri libri in librat)
+        {
+            if (libri.Viti > 2015)
+            {
+                Console.WriteLine(libri.Titulli);
+            }
+        }
+    }
+}
---
+++ __Leksione/listat/listat/Program.cs
@@ -0,0 +1,19 @@
+﻿//krijo nje klase Libri me fushat titulli, autori , viti
+//Krijo nje list me libra
+//me nje metode afisho() afishoni te gjithe librat qe jane publikuar pas vitit 2015
+List<Libri> librat = new List<Libri>
+{
+    new Libri
+    {
+        Autori="Naim", Viti= 2016, Titulli="testi1"
+    },new Libri
+    {
+        Autori="Lasgushi", Viti= 2008, Titulli="testi2"
+    },new Libri
+    {
+        Autori="Kadare", Viti= 2025, Titulli="testi3"
+    },
+};
+Libri.Afisho(librat);
+
+
